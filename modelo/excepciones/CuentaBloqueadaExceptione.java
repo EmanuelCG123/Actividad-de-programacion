ccd5f670e78944e70536e49f0e986a2c3fd2eea6
EmanuelCG123 <the438130@gmail.com>
2026-04-10 12:43:42 -0500

    Delete modelo directory

--- modelo/excepciones/CuentaBloqueadaExceptione.java
+++
@@ -1,8 +0,0 @@
-package modelo.excepciones;
-
-public class CuentaBloqueadaExceptione extends SistemaBancarioException {
-
-    public CuentaBloqueadaExceptione(String numeroCuenta) {
-        super("La cuenta " + numeroCuenta + " está bloqueada.", "CUENTA-001");
-    }
-}
